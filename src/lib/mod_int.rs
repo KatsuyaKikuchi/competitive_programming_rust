af60e935f4e1974f1b9831e91aec78a94e34c287
KatsuyaKikuchi <k.kikuchi.ah@gmail.com>
2021-11-01 23:16:52 +0900

    modintと他の型の演算を定義中

--- src/lib/mod_int.rs
+++ src/lib/mod_int.rs
@@ -1,31 +1,84 @@
-pub struct ModInt {
-    value: i64,
-}
+use std::ops;
 
-impl ModInt {
-    // version 1.5でconst genericsに移せる
-    // https://qiita.com/namn1125/items/5100cb85021a1d6e8f6c
-    const MOD: i64 = 1000_000_007;
+pub mod mod_int {
+    type ModInternalType = i64;
 
-    fn new(value: i64) -> Self {
-        let v = if value < 0 {
-            Self::MOD - ((-value) % Self::MOD)
-        } else {
-            value
-        };
+    pub struct ModInt {
+        pub value: ModInternalType,
+    }
 
-        let v = if v >= Self::MOD {
-            v % Self::MOD
-        } else {
-            v
-        };
+    impl ModInt {
+        // version 1.5でconst genericsに移せる
+        // https://qiita.com/namn1125/items/5100cb85021a1d6e8f6c
+        pub const MOD: ModInternalType = 1000_000_007;
 
-        ModInt {
-            value: v
+        pub fn new<T: ToInternalNum>(value: T) -> Self {
+            let value = value.to_internal_num();
+            let v = if value < 0 {
+                Self::MOD - ((-value) % Self::MOD)
+            } else {
+                value
+            };
+
+            let v = if v >= Self::MOD {
+                v % Self::MOD
+            } else {
+                v
+            };
+
+            ModInt {
+                value: v
+            }
+        }
+    }
+
+    // 整数型をModIntとの演算に使用できるようにするためのトレイト
+    pub trait ToInternalNum {
+        fn to_internal_num(&self) -> ModInternalType;
+    }
+
+    impl ToInternalNum for ModInt {
+        fn to_internal_num(&self) -> ModInternalType {
+            self.value
+        }
+    }
+
+    macro_rules! impl_primitive {
+        ($primitive:ident)=>{
+            impl ToInternalNum for $primitive{
+                fn to_internal_num(&self) ->ModInternalType{
+                    *self as ModInternalType
+                }
+            }
+        }
+    }
+
+    impl_primitive!(i32);
+
+    // 四則演算
+    impl<T: ToInternalNum> std::ops::AddAssign<T> for ModInt {
+        fn add_assign(&mut self, rhs: T) {
+            let mut rhs = rhs.to_internal_num();
+            let modulo = ModInt::MOD;
+            if rhs >= modulo {
+                rhs %= modulo;
+            }
+            self.value += rhs;
+            if self.value >= modulo {
+                self.value -= modulo;
+            }
+        }
+    }
+
+    impl<T: ToInternalNum> std::ops::Add<T> for ModInt {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let mut res = self;
+            res += other;
+            res
         }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -33,9 +86,10 @@ mod tests {
 
     #[test]
     fn mod_int_works() {
-        let a = ModInt::new(10);
+        let mut a = mod_int::ModInt::new(10);
         assert_eq!(a.value, 10);
-        let b = ModInt::new(-1);
-        assert_eq!(b.value, ModInt::MOD - 1);
+        let mut b = mod_int::ModInt::new(-1);
+        assert_eq!(b.value, mod_int::ModInt::MOD - 1);
+        assert_eq!((a + b).value, 9)
     }
 }
